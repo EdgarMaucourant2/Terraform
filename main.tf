b81ae47e03fc886c08e82c39ff3bc13c7089d52f
EdgarMaucourant2 <94912393+EdgarMaucourant2@users.noreply.github.com>
2021-11-30 12:13:05 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-# main.tf
-# Configure the Microsoft Azure Provider
+# main.tf
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
       source  = "hashicorp/azurerm"
       version = "=2.46.0"
-    }
+    }
   }
 }
 
-# Create a resource group
+# Create a resource group
 resource "azurerm_resource_group" "test-rg" {
   name     = "${var.prefix}-resources";
   location = "${var.location}";
