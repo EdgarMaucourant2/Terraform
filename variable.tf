6530010ab4e6eed8e317c2088576d77b3a0cc133
EdgarMaucourant2 <94912393+EdgarMaucourant2@users.noreply.github.com>
2021-11-30 11:59:38 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,13 +3,13 @@ variable "client_secret" {
   description = "Enter your Client Secret. Please make sure you do not store the value of your client secret in the SCM repository"
 }
 variable "client_id" {
-  description = "5f7ba26e-4e24-4504-8949-c7a4a8760c84"
+  description = "Your Client ID"
 }
 variable "subscription_id" {
-  description = "c20ab15d-6cd7-4a9e-b070-eced6fc72216"
+  description = "Your Subscription ID"
 }
 variable "tenant_id" {
-  description = "755b9019-04e8-407c-aefc-aba24f12ab71"
+  description = "Your Tenant ID"
 }
 variable "prefix" {
   description = "The prefix which should be used for all resources in this Chapter"
